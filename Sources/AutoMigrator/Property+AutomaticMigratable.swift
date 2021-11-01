9e2bdf8e684480d2a9598abff9242d269b8e65a2
Mats Eikeland Mollestad <mem@mollestad.no>
2021-11-01 19:54:13 +0100

    public protocol conformance

--- Sources/AutoMigrator/Property+AutomaticMigratable.swift
+++ Sources/AutoMigrator/Property+AutomaticMigratable.swift
@@ -21,32 +21,32 @@ func dataType<T>(from type: T.Type) -> DatabaseSchema.DataType {
     }
 }
 
-public extension FieldProperty: AutomaticMigratable {
-    var addMigration: String { ".field(\"\(fieldName)\", .\(dataType(from: Value.self)), .required)" }
-    var fieldName: String { key.description }
+extension FieldProperty: AutomaticMigratable {
+    public var addMigration: String { ".field(\"\(fieldName)\", .\(dataType(from: Value.self)), .required)" }
+    public var fieldName: String { key.description }
 }
 
-public extension OptionalFieldProperty: AutomaticMigratable {
-    var addMigration: String { ".field(\"\(fieldName)\", .\(dataType(from: WrappedValue.self)))" }
-    var fieldName: String { key.description }
+extension OptionalFieldProperty: AutomaticMigratable {
+    public var addMigration: String { ".field(\"\(fieldName)\", .\(dataType(from: WrappedValue.self)))" }
+    public var fieldName: String { key.description }
 }
 
-public extension IDProperty: AutomaticMigratable {
-    var addMigration: String { ".field(\"\(fieldName)\", .\(dataType(from: Value.self)), .identifier(auto: false))" }
-    var fieldName: String { key.description }
+extension IDProperty: AutomaticMigratable {
+    public var addMigration: String { ".field(\"\(fieldName)\", .\(dataType(from: Value.self)), .identifier(auto: false))" }
+    public var fieldName: String { key.description }
 }
 
-public extension TimestampProperty: AutomaticMigratable {
-    var addMigration: String { ".field(\"\(fieldName)\", .datetime)" }
-    var fieldName: String { $timestamp.key.description }
+extension TimestampProperty: AutomaticMigratable {
+    public var addMigration: String { ".field(\"\(fieldName)\", .datetime)" }
+    public var fieldName: String { $timestamp.key.description }
 }
 
-public extension ParentProperty: AutomaticMigratable {
-    var addMigration: String { ".field(\"\(fieldName)\", .\(dataType(from: To.IDValue.self)), .required, .references(\"\(To.schema)\", .id, onDelete: .cascade, onUpdate: .cascade))" }
-    var fieldName: String { $id.key.description }
+extension ParentProperty: AutomaticMigratable {
+    public var addMigration: String { ".field(\"\(fieldName)\", .\(dataType(from: To.IDValue.self)), .required, .references(\"\(To.schema)\", .id, onDelete: .cascade, onUpdate: .cascade))" }
+    public var fieldName: String { $id.key.description }
 }
 
-public extension OptionalParentProperty: AutomaticMigratable {
-    var addMigration: String { ".field(\"\(fieldName)\", .\(dataType(from: To.IDValue.self)), .references(\"\(To.schema)\", .id, onDelete: .cascade, onUpdate: .cascade))" }
-    var fieldName: String { $id.key.description }
+extension OptionalParentProperty: AutomaticMigratable {
+    public var addMigration: String { ".field(\"\(fieldName)\", .\(dataType(from: To.IDValue.self)), .references(\"\(To.schema)\", .id, onDelete: .cascade, onUpdate: .cascade))" }
+    public var fieldName: String { $id.key.description }
 }
